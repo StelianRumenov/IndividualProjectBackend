b948ed6ed7f3f57cad9b26643c805c8f8a510ba0
Stelian Rumenov <stelian@duodeka.nl>
2024-01-15 16:15:30 +0100

    disabled cors

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const port = parseInt(process.env.PORT) || 3002;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    credentials: true,
-    origin: 'http://localhost:5173',
-  });
+  // app.enableCors({
+  //   credentials: true,
+  //   origin: 'http://localhost:5173', 'http://localhost:3000
+
+  // });
 
   // const config = app.get(ConfigService);
   app.use(cookieParser());
