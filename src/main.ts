5fcba854b0e923bdd09c915f86a33cc03664d82b
Stelian Rumenov <stelian@duodeka.nl>
2024-01-15 16:20:40 +0100

    lets see

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const port = parseInt(process.env.PORT) || 3002;
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   credentials: true,
-  //   origin: 'http://localhost:5173', 'http://localhost:3000
-
-  // });
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+  });
 
   // const config = app.get(ConfigService);
   app.use(cookieParser());
